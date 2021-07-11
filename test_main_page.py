00efefd22db31f0efa323be4f07caccd5157ea21
Anderson <darkvenom32@outlook.com>
2021-07-11 16:08:37 +0300

    Fixed import in main_page, removed method of navigation from test_main_page, added new logic

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    navigate_to_login_page(browser)
-
-
-def navigate_to_login_page(browser):
-    login_url = browser.find_element_by_css_selector("#login_link")
-    login_url.click()
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.go_to_login_page()
