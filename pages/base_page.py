da83587dc34eb890bc0c4a27dd9b47e64a510f8b
Anderson <darkvenom32@outlook.com>
2021-07-11 17:07:03 +0300

    Added is_part_of_url function to base class, added locators for login page, added assert logic to login_page.py, added test case to test_main_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,14 +7,18 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-
     def open(self):
         self.browser.get(self.url)
-
 
     def is_element_presented(self, method, element):
         try:
             self.browser.find_element(method, element)
-        except(NoSuchElementException):
+        except NoSuchElementException:
             return False
         return True
+
+    def is_part_of_url(self, part):
+        if part in self.browser.current_url:
+            return True
+        else:
+            return False
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -7,14 +7,21 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
+
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        url = self.browser.current_url
+        assert self.is_part_of_url("login"),\
+            f"Expected login page, but got {url}"
+
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_presented(*LoginPageLocators.LOGIN_FORM), \
+            "Expected Login form, but found nothing"
+
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_presented(*LoginPageLocators.REGISTER_FORM),\
+            "Expected Registerform, but found nothing"
