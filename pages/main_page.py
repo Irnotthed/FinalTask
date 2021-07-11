1480aa6026700105cc5a2875a3abbea43806169b
Anderson <darkvenom32@outlook.com>
2021-07-11 16:27:01 +0300

    Added locators file, changed logic of main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,14 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
 
     def should_be_login_url(self):
-        assert self.is_element_presented(By.CSS_SELECTOR, "#login_link"),\
+        assert self.is_element_presented(*MainPageLocators.LOGIN_LINK),\
             "Login link is not presented"
